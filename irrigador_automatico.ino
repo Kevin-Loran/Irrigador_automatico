e1003562d4221e5c2d905a4b45fff9a84c263e71
Kevin Loran de Oliveira Manoel <kevinLoran.eng@gmail.com>
2026-08-16 18:25:02 -0300

    Create irrigador_automatico.ino

---
+++ irrigador_automatico.ino
@@ -0,0 +1,21 @@
+#define rele 3
+#define sensor 2
+
+bool irrigar = false;
+
+void setup() {
+    pinMode(rele, OUTPUT);
+    pinMode(sensor, INPUT);
+}
+
+void loop() {
+    irrigar = digitalRead(sensor);
+
+    if (irrigar) {
+        digitalWrite(rele, LOW);
+    } else {
+        digitalWrite(rele, HIGH);
+    }
+
+    delay(500);
+}
